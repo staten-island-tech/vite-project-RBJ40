e91fdb7dcd9e8d6c7bfb7871de767b3560fd4833
RBJ40 <celinechan268@gmail.com>
2024-11-06 12:26:49 -0500

    draft

---
+++ vite-project/JS/index.js
@@ -0,0 +1,13 @@
+document.querySelector("lightbutton").addEventListener("click", function () {});
+const games = {
+    name: "Street Fighter",
+    price: "$30",
+    category: "Action",
+ 
+},
+{
+    name: "Tears of the Kingdom",
+    price: "$60",
+    category: "Adventure",
+},
+
