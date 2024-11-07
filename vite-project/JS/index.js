538bf55e1eaca370d732465112e6d629edab8260
RBJ40 <celinechan268@gmail.com>
2024-11-06 23:30:45 -0500

    draft

--- vite-project/JS/index.js
+++ vite-project/JS/index.js
@@ -1,13 +1,20 @@
 document.querySelector("lightbutton").addEventListener("click", function () {});
-const games = {
-    name: "Street Fighter",
-    price: "$30",
-    category: "Action",
- 
-},
-{
-    name: "Tears of the Kingdom",
-    price: "$60",
-    category: "Adventure",
-},
-
+const games = [
+    {name: "Tears of the Kingdom", price: "$60", category: "Adventure"},
+    {name: "Street Fighter", price: "$30", category: "Action"},
+    {name: "Breath of the Wild", price: "$50", category: "Adventure"},
+    {name: "Mario Kart 8 Deluxe", price: "$50", category: "Action"},
+    {name: "Tekken 8", price: "$60", category: "Action"},
+    {name: "Super Smash Bros Ultimate", price: "$50", category: "Action"},
+    {name: "Naruto Ultimate Ninja Storm", price: "$50", category: "Action"},
+    {name: "Mario Kart Double Dash", price: "$30", category: "Action"},
+    {name: "Kirby and the Forgotten Land", price: "$40", category: "Adventure"},
+    {name: "Kirby Star Allies", price: "$40", category: "Adventure"},
+    {name: "Kirby's Dream Land", price: "$40", category: "Adventure"},
+    {name: "Kirby Planet Robobot", price: "$40", category: "Adventure"},
+    {name: "Kid Icarus: Uprising", price: "$40", category: "Action"},
+    {name: "Wii Sports", price: "$30", category: "Action"},
+    {name: "Skyward Sword", price: "$50", category: "Adventure"},
+];
+games.forEach(game) => console.log(student);
+const adventureGame = games.filter((games) => games.category == "Adventure");
