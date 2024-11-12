a3adb6901e744b2d015fbda1d50c5d0e1ac8e795
RBJ40 <celinechan268@gmail.com>
2024-11-12 12:26:48 -0500

    draft

---
+++ vite-project/JS/DOM.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import "../CSS/style.css";
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  description: document.querySelector(".card-desc"),
+  items: document.querySelectorAll("ul"),
+  button: document.querySelectorAll("button"),
+  form: document.querySelectorAll(".form"),
+  container: document.querySelectorAll(".container"),
+  card: document.querySelector(".card"),
+};
